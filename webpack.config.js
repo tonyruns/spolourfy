12649321426baf641c3d421546eb430b24976383
eho <ernestho.ho@gmail.com>
2016-10-01 18:38:50 -0400

    add dev proxy

--- webpack.config.js
+++ webpack.config.js
@@ -27,5 +27,13 @@ module.exports = {
       }
     ]
   },
-  plugins: []
+  plugins: [],
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }
+  }
 };
