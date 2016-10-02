8e3ebae65be539d76b39c8a8861c8abc6cbb4c3f
Zal Machado <zalmachado@gmail.com>
2016-10-02 18:03:55 -0400

    get album colours

--- app/actions/colorActions.js
+++ app/actions/colorActions.js
@@ -41,6 +41,23 @@ export function getAlbumColours() {
   };
 }
 
+export function updatePlaylistColors() {
+  return (dispatch, getState) => {
+    const { colors, albums } = getState();
+    const existingUrls = Object.keys(colors);
+    const newUrls = albums.albums.items.map(item => item.track.album.images[0]).filter(image => image).map(image => image.url);
+
+    if (newUrls.length == 0)
+      return;
+
+    return getColors(newUrls)
+      .then(data => {
+        dispatch({ type: ALBUM_COLOURS_SUCCESS, data });
+        return data;
+      });
+  };
+}
+
 function getPlaylistTracksImageUrls(playlist) {
   const trackItems = playlist.tracks.items;
   return trackItems.map(item => item.track.album.images[0]).filter(image => image).map(image => image.url);
